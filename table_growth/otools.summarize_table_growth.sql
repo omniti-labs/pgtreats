7a28b06cb99ad2da03d8b72c534ce76ec1da5745
Robert Treat <robert@omniti.com>
2008-09-04 17:36:40 +0000

    fix errors caused by adding new tables to schema. fix duplicate rows in monthly summary output

--- table_growth/otools.summarize_table_growth.sql
+++ table_growth/otools.summarize_table_growth.sql
@@ -8,7 +8,7 @@ begin
 -- Daily summarization
 IF to_char(current_date,'dd') <> '01' THEN
     insert into otools.table_growth (table_owner, schema_name, table_name, actual_size, growth_size, sum_flag, capture_time)
-    select a.table_owner, a.schema_name, a.table_name, a.actual_size, a.actual_size-b.actual_size AS table_growth, 1, a.capture_time
+    select a.table_owner, a.schema_name, a.table_name, a.actual_size, a.actual_size-coalesce(b.actual_size,0) AS table_growth, 1, a.capture_time
     from otools.table_growth a 
         left join otools.table_growth b 
             on (a.table_owner=b.table_owner and a.table_name=b.table_name and a.schema_name=b.schema_name and b.capture_time = current_date -1) 
@@ -21,7 +21,7 @@ END IF;
 -- Monthly summarization
 IF to_char(current_date,'dd') = '01' THEN
     insert into otools.table_growth (table_owner, schema_name, table_name, actual_size, growth_size, sum_flag, capture_time)
-    select a.table_owner, a.schema_name, a.table_name, actual_size, sum(growth_size), 2, (current_date - '1 month'::interval) 
+    select a.table_owner, a.schema_name, a.table_name, max(actual_size), sum(growth_size), 2, (current_date - '1 month'::interval) 
     from otools.table_growth a 
     where sum_flag=1 and capture_time between (current_date - '1 month'::interval) and current_date 
     group by table_owner, schema_name, table_name, actual_size;
