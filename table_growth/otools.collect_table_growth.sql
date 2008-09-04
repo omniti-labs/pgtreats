722c6e310e53f7741e8aebe05a0bf5851f4510e8
Robert Treat <robert@omniti.com>
2008-09-04 17:42:26 +0000

    lower the limit to 10000 pages for table to track

--- table_growth/otools.collect_table_growth.sql
+++ table_growth/otools.collect_table_growth.sql
@@ -3,5 +3,5 @@ returns void
 as $$
     insert into otools.table_growth (table_owner, schema_name, table_name, actual_size, growth_size, sum_flag, capture_time) 
     select pg_get_userbyid(c.relowner) AS table_owner, n.nspname AS schema_name, c.relname AS table_name, pg_total_relation_size(c.oid), 0, 0, current_date 
-    from pg_class c JOIN pg_namespace n ON (c.relnamespace=n.oid) where relkind = 'r' and reltuples > 25000;
+    from pg_class c JOIN pg_namespace n ON (c.relnamespace=n.oid) where relkind = 'r' and reltuples > 10000;
 $$ language sql;
