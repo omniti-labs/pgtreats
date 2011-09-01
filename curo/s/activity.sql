26c12de4008352b9a117c9b722fd8e32407376c0
Depesz Lubaczewski <depesz@omniti.com>
2011-09-01 22:40:49 +0000

    another batch of ~ to be removed

--- curo/s/activity.sql
+++ curo/s/activity.sql
@@ -1,5 +1,5 @@
 -- Current activity
-\! $HOME/.curo/c/watch $HOME/.curo/s/activity/query.1.sql ~/.curo/s/activity/fifo "Current activity"
+\! $HOME/.curo/c/watch $HOME/.curo/s/activity/query.1.sql $HOME/.curo/s/activity/fifo "Current activity"
 \i ~/.curo/s/activity/fifo
-\! rm -f ~/.curo/s/activity/fifo
+\! rm -f $HOME/.curo/s/activity/fifo
 \! clear
--- curo/s/locks.sql
+++ curo/s/locks.sql
@@ -1,5 +1,5 @@
 -- Locks information
-\! $HOME/.curo/c/watch $HOME/.curo/s/locks/locks.sql ~/.curo/s/locks/fifo "Locks information"
+\! $HOME/.curo/c/watch $HOME/.curo/s/locks/locks.sql $HOME/.curo/s/locks/fifo "Locks information"
 \i ~/.curo/s/locks/fifo
-\! rm -f ~/.curo/s/locks/fifo
+\! rm -f $HOME/.curo/s/locks/fifo
 \! clear
